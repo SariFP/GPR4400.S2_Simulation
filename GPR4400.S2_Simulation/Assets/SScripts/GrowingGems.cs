5670c29c081e4e2a20c0e8c0cc8803a01f095bbf
Lena James <lena.biresch@gmx.de>
2019-06-05 14:53:56 +0200

    Updated Gem

--- GPR4400.S2_Simulation/Assets/SScripts/GrowingGems.cs
+++ GPR4400.S2_Simulation/Assets/SScripts/GrowingGems.cs
@@ -6,7 +6,6 @@ public class GrowingGems : MonoBehaviour
 {
     public GameObject GemPrefab;
 
-    [SerializeField]
     private GameObject newGem;
 
 
@@ -21,13 +20,8 @@ public class GrowingGems : MonoBehaviour
     {
         for (float f = 0f; f <= 3.5; f += 0.01f)
         {
-
             newGem.transform.localScale = new Vector3(f, f, f);
-
             yield return new WaitForSeconds(.1f);
         }
-
-        //yield return new WaitForSeconds(5);
-        //newGem.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
     }
 }
