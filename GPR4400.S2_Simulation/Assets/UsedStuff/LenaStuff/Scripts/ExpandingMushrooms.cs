fa0665c06f437efa42eb0507a232510f7f3244ae
Lena James <lena.biresch@gmx.de>
2019-06-11 15:02:13 +0200

    [ADDED] Bodenpilz

---
+++ GPR4400.S2_Simulation/Assets/UsedStuff/LenaStuff/Scripts/ExpandingMushrooms.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandingMushrooms : MonoBehaviour
+{
+
+
+    public GameObject MushroomPrefab;
+    public int xMin;
+    public int xMax;
+    public int zMin;
+    public int zMax;
+    private int terrainSize = 100;
+
+    private float[,] terrain;
+    private float x;
+    private float z;
+
+    private Vector3 position;
+    private List<Vector3> ExistingMushrooms;
+    private List<Vector3> ExistingNeighbors;
+    private List<Vector3> OutsideTheBox;
+    private List<Vector3> InsideButFree;
+    private List<Vector3> hikingPoints;
+
+    private Vector3 hikingPoint;
+    private System.Random rand;
+
+    void Start()
+    {
+        ExistingMushrooms = new List<Vector3>();
+        ExistingNeighbors = new List<Vector3>();
+        OutsideTheBox = new List<Vector3>();
+        hikingPoints = new List<Vector3>();
+        terrain = new float[100, 100];
+        rand = new System.Random();
+        x = rand.Next(xMin, xMax);
+        z = rand.Next(zMin, zMax);
+        hikingPoint = new Vector3(x, 0, z);
+        hikingPoints.Add(hikingPoint);
+    }
+
+    void Update()
+    {
+        if (!ExistingNeighbors.Contains(hikingPoint))
+        {
+            SentHikingPoint();
+        }
+        else
+        {
+            Instantiate(MushroomPrefab, hikingPoint, Quaternion.identity);
+        }
+    }
+
+
+    private void SentHikingPoint()
+    {
+        int q = Random.Range(1, 5);
+        Debug.Log("q= " + q);
+        if (q == 1)
+        {
+            hikingPoint += new Vector3(0.02f, 0, 0);
+            Instantiate(MushroomPrefab, hikingPoint, Quaternion.identity);
+        }
+        else if (q == 2)
+        {
+            hikingPoint += new Vector3(0, 0, -0.02f);
+            Instantiate(MushroomPrefab, hikingPoint, Quaternion.identity);
+        }
+        else if (q == 3)
+        {
+            hikingPoint += new Vector3(-0.02f, 0, 0);
+            Instantiate(MushroomPrefab, hikingPoint, Quaternion.identity);
+        }
+        else if (q == 4)
+        {
+            hikingPoint += new Vector3(0, 0, 0.02f);
+            Instantiate(MushroomPrefab, hikingPoint, Quaternion.identity);
+        }
+    }
+}
