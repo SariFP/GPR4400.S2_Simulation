8add959cb0dc0527b0e9095013d7373dadcef51d
Lena James <lena.biresch@gmx.de>
2019-06-13 18:11:02 +0200

    [ADDED] SimulationManager as Singleton

---
+++ GPR4400.S2_Simulation/Assets/UsedStuff/LenaStuff/Scripts/SimulationManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationManager : MonoBehaviour
+{
+    private static SimulationManager instance;
+
+    public static SimulationManager Instance { get { return instance; } }
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
